b487d3b150f9315bf6f94b283acc171a699b630f
Cheirsh <shmilycherish@gmail.com>
2020-07-02 21:43:47 +0800

    [#9]change functional components to class components

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Number from "./components/Number";
 
-function App(props) {
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <Number {...props}></Number>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <Number {...this.props}></Number>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
